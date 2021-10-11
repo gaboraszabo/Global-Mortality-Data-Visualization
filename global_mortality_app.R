c0115e78a8f11343e5acac7e316d8642c0fef155
Gabor Szabo <85987675+gaborszabo11@users.noreply.github.com>
2021-10-11 11:20:44 -0700

    Latest
    Brush functions, pareto and NaN problems fixed

--- global_mortality_app.R
+++ global_mortality_app.R
@@ -76,7 +76,9 @@ ui <- navbarPage("Global Mortality App",
                offset = 0,
                
                # small multiples plot
-               plotOutput("small_multiples", width = "800px", height = "660px")
+               plotOutput("small_multiples",
+                          width = "800px",
+                          height = "660px")
                
         )
     )
@@ -185,6 +187,54 @@ ui <- navbarPage("Global Mortality App",
                         
                  )
              )
+    ),
+
+    # Tab 4 ----
+    tabPanel("Pareto of causes by country",
+            # Application title
+         
+            fluidRow(
+             
+                # first column
+                column(5,
+                        hr(),
+                        sidebarPanel(width = 6,
+                                                  
+                                                  
+                                    sliderInput(inputId = "year_range", 
+                                                label   = "Date range:",
+                                                min     = 1990, 
+                                                max     = 2016,
+                                                value   = c(1990, 2016)
+                                                )
+                                    ),
+                                    
+                       sidebarPanel(width = 6,
+                                    
+                                    selectInput(inputId = "country_4",
+                                                label      = "Select country", 
+                                                choices    = countries, 
+                                                selected   = c("United States"), 
+                                                multiple   = FALSE, 
+                                                selectize  = FALSE,
+                                                size = 30
+                                                )
+                                    )
+                       
+                ),
+             
+                # second column
+                column(7, 
+                    offset = 0,
+                    
+                    # plot
+                    plotOutput("pareto",
+                               width  = "800px",
+                               height = "650px"),
+                    
+                        )
+                
+        )
     )
 )
 
@@ -416,8 +466,64 @@ server <- function(input, output) {
         }
     })
 
+
     
     
+# Pareto ----    
+    
+    output$pareto <- renderPlot({
+        
+        mortality_tbl_long %>% 
+            
+            filter(country == input$country_4) %>% 
+            filter(year >= range(input$year_range)[1] & year <= range(input$year_range)[2]) %>%
+            filter(!is.na(proportion)) %>% 
+            
+            group_by(cause) %>% 
+            summarize(proportion = mean(proportion, na.rm = TRUE)) %>% 
+            ungroup() %>%
+            
+            mutate(cause = cause %>% as_factor() %>% fct_reorder(proportion)) %>% 
+            mutate(cause_fct_num = cause %>% as_factor() %>% fct_reorder(desc(proportion)) %>% as.numeric()) %>%  
+            
+            mutate(category = case_when(cause_fct_num < 4 ~ "top 3",
+                                        cause_fct_num < 11 ~ "top 10",
+                                        TRUE ~ "the rest") %>% as_factor() %>% fct_reorder(desc(cause_fct_num))) %>% 
+            
+            ggplot(aes(cause, proportion)) +
+            geom_col(aes( fill = category)) +
+            geom_text(aes(label = proportion %>% scales::percent(accuracy = 0.01)),
+                      position = position_dodge(width = 0.2),
+                      hjust = -0.2, 
+                      size = 4, 
+                      color = "grey30"
+                      ) +
+            coord_flip() +
+            
+            theme_light() +
+            theme(panel.grid.major.y = element_blank(),
+                  panel.grid.minor   = element_blank(),
+                  axis.title         = element_text(color = "grey30"),
+                  axis.text          = element_text(color = "grey30", size = 11),
+                  plot.title         = element_text(size = 20, face = "bold", color = "grey30"),
+                  plot.subtitle      = element_text(size = 15, face = "bold", color = "grey30")
+            ) +
+            labs(
+                title    = "Pareto Chart of Causes by Country",
+                subtitle = input$country_4,
+                x        = "Cause", 
+                y        = "Proportion (% of overall deaths)") +
+            guides(fill = guide_legend(reverse = TRUE)) +
+            scale_y_continuous(
+                labels = scales::percent_format(accuracy = 1),
+                breaks = seq(0, 1, by = 0.1),
+                limits = c(0, 0.65)
+                ) +
+            scale_fill_brewer(palette = "Blues")
+        
+        
+        
+    })
        
     }
 
